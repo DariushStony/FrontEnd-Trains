98b155eeeb8862f3cb49761434f7a972caf90b35
Dariush Hadipour <dariushhadi87@gmail.com>
2021-04-26 10:48:18 +0430

    State and Lifecycle

--- React_Redux/React/temp.jsx
+++ React_Redux/React/temp.jsx
@@ -1,20 +1,37 @@
-// Function Component
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
+class Clock extends React.Component {
 
-function App() {
-    return (
-        <div>
-            <Welcome name="Dariush" />
-            <Welcome name="Kianoosh" />
-            <Welcome name="Abtin" />
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    //Lifecycle Methods
+    componentDidMount() {
+        this.timerID = setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-    <App />,
+    <Clock />,
     document.getElementById("root")
 );
-

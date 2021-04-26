827c015872aff0b0e18d51fd9faffdf688d81051
Dariush Hadipour <dariushhadi87@gmail.com>
2021-04-26 09:18:03 +0430

    Rendering Elements

--- React_Redux/React/temp.js
+++ React_Redux/React/temp.js
@@ -1,10 +1,21 @@
-var name = "Dariush Hadipour";
-var element = React.createElement(
-    "h1",
-    null,
-    "Hello, ",
-    name,
-    "!"
-);
+function tick() {
+    var element = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Hello, World!"
+        ),
+        React.createElement(
+            "h2",
+            null,
+            "It is ",
+            new Date().toLocaleTimeString(),
+            "."
+        )
+    );
+    ReactDOM.render(element, document.getElementById("root"));
+}
 
-ReactDOM.render(element, document.getElementById("root"));
+setInterval(tick, 1000);
--- React_Redux/React/temp.jsx
+++ React_Redux/React/temp.jsx
@@ -1,7 +1,14 @@
-const name = "Dariush Hadipour";
-const element = <h1>Hello, {name}!</h1>;
+function tick(){
+    const element = (
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById("root")
+    );
+}
 
-ReactDOM.render(
-    element,
-    document.getElementById("root")
-);
+setInterval(tick, 1000);

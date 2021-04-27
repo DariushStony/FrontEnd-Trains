08a398cef81a559a2789e7799276b5d9370849b2
Dariush Hadipour <dariushhadi87@gmail.com>
2021-04-27 11:47:13 +0430

    before State

--- React_Redux/react-app-test/src/index.js
+++ React_Redux/react-app-test/src/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const myfirstelement = <h1>Hello React!</h1>;
+
+// function Car() {
+//     return <h2>Hi, I am also a Car!</h2>;
+// }
+
+class Car extends React.Component {
+
+    constructor() {
+        super();
+        // this.state = { color: "red" }
+    }
+
+    render() {
+        return <h2>Hi, I am a {this.props.color} Car!</h2>;
+    }
+}
+
+class Garage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Who lives in my garage?</h1>
+                <Car />
+                <App />
+            </div>
+        );
+    }
+}
 
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(
+    <Garage />,
+    document.getElementById("root")
+);
